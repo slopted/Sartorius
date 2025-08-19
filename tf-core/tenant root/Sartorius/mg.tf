1eb6c15c37c2d3638649fadb15d8d2db272e3f98
asanchezd8_bch <asanchezd8.xms@bancochile.cl>
2025-08-19 15:51:27 -0400

    feat: MG 2

--- tf-core/tenant root/Sartorius/mg.tf
+++ tf-core/tenant root/Sartorius/mg.tf
@@ -1,4 +1,11 @@
-module "Sartorius" {
-  source = "::git:///tf-module/Management_Group"
+module "MG_Lvl1" {
+  source = "git::https://github.com/slopted/Sartorius.git//tf-modules/Managment_Group?ref=v1.0.0"
   display_name = var.display_name_mg_lvl1
+}
+
+module "MG_Lvl2" {
+  source = "git::https://github.com/slopted/Sartorius.git//tf-modules/Managment_Group?ref=v1.0.0"
+  for_each = var.display_name_mg_lvl2
+  display_name = each.value
+  parent_management_group_id = module.MG_Lvl1.id
 }
